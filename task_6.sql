2a1f317e2190cc8eedb076495669588021fa520f
kevinmutwiri <keivnmutwiri@gmail.com>
2025-06-28 20:50:34 +0300

    Refactor

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,6 @@
+-- Inserts multiple rows into the 'customer' table in the alx_book_store database.
+
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
