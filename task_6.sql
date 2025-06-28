3f26244fcd4940adce1cbeb7355439207e95985c
Kevin Mutwiri <keivnmutwiri@gmail.com>
2025-06-28 20:51:59 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,3 @@
--- Inserts multiple rows into the 'customer' table in the alx_book_store database.
-
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
